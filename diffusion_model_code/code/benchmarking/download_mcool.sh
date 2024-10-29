3c16e77a48ad1a697643e355d4b5db7a4ea662d9
gschuette <85568979+gschuette@users.noreply.github.com>
2024-10-29 19:00:27 -0400

    Batch 4

---
+++ diffusion_model_code/code/benchmarking/download_mcool.sh
@@ -0,0 +1,11 @@
+
+# The AWS location of the mcool file associated with the 4DN-processed mcool contact matrix for GM12878 cells from https://doi.org/10.1016/j.cell.2014.11.021
+# Experiments Main Page: https://data.4dnucleome.org/experiment-set-replicates/4DNES3JX38V5/
+# File Main Page: https://data.4dnucleome.org/files-processed/4DNFIXP4QG5B/
+# Accession: 4DNFIXP4QG5B
+# Note: 4DN processed the data using the hg38 (AKA GRCh38) genome assembly, while the original source
+curl -o ../../data/outside/GM12878_hg38.mcool https://4dn-open-data-public.s3.amazonaws.com/fourfront-webprod/wfoutput/d6abea45-b0bb-4154-9854-1d3075b98097/4DNFIXP4QG5B.mcool
+
+
+
+
